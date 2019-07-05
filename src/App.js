5b5f2f5c16e4aff667b745ca95ca8dbd0d6af0db
Casey Heagerty <cheagerty@gmail.com>
2019-07-05 15:51:31 +0200

    my-travels react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React,  { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Travel from "./Travel";
+
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">My Travels</h1>
+          </header>
+          <Travel
+              destination="Kinsale"
+              country="Ireland"
+              photo="https://d3dqioy2sca31t.cloudfront.net/Projects/cms/production/000/024/002/large/29389e7762514c88846da0ae79c22006/ireland-kinsale-town-052418-po.jpg"
+              distance="800 miles"
+          />
+          <Travel
+              destination="Rome"
+              country="Italy"
+              photo="https://lonelyplanetimages.imgix.net/mastheads/stock-photo-roman-sunset-77415821.jpg?sharp=10&vib=20&w=1200"
+              distance="1000 miles"
+          />
+        </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Travel.js
@@ -0,0 +1,15 @@
+// src/Travel.js
+
+import React from "react";
+
+const Travel = ({ destination, country, photo, distance }) => (
+    <figure>
+        <img src={photo} alt={destination} />
+        <figcaption>
+            <blockquote>{country}</blockquote>
+            <cite>{distance}</cite>
+        </figcaption>
+    </figure>
+);
+
+export default Travel;
